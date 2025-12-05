2c34e9364b96e508e3ebb1f2285a0a1f857d4816
seoyoon lee <leeseoyoon01@gmail.com>
2025-12-05 10:58:12 +0900

    feat: 결제 페이지 구현

--- app/src/main/java/com/example/movieapplication/PayActivity.java
+++ app/src/main/java/com/example/movieapplication/PayActivity.java
@@ -1,18 +1,21 @@
 package com.example.movieapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.movieapplication.api.UseMovieDetailApi;
 import com.example.movieapplication.components.NavigationBar;
 import com.example.movieapplication.data.MovieCost;
+import com.example.movieapplication.db.UserTicketQuery;
 import com.example.movieapplication.domain.Ticket;
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -24,13 +27,15 @@ import java.util.Map;
 public class PayActivity extends AppCompatActivity {
     Map<String, Integer> costMap;
     Ticket ticket;
+    int id;
+    int clickedRes = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
 
         Intent prevIntent = getIntent();
-        int id = prevIntent.getIntExtra("id",0);
+        id = prevIntent.getIntExtra("id",0);
         String theater = prevIntent.getStringExtra("theater");
         String date = prevIntent.getStringExtra("date");
         String time = prevIntent.getStringExtra("time");
@@ -79,6 +84,10 @@ public class PayActivity extends AppCompatActivity {
 
             // 클릭한 버튼만 선택 상태 (빨간 테두리)
             clicked.setBackgroundResource(R.drawable.border_red);
+
+            if (clicked == lpay) clickedRes = R.drawable.lpay;
+            else if (clicked == toss) clickedRes = R.drawable.toss;
+            else if (clicked == naver) clickedRes = R.drawable.naver;
         };
 
         lpay.setOnClickListener(payClickListener);
@@ -96,6 +105,18 @@ public class PayActivity extends AppCompatActivity {
     }
 
     public void moveToPayDone(View view){
-        // 저장 로직
+        SharedPreferences prefs = getSharedPreferences("app_prefs", MODE_PRIVATE);
+        int userId = prefs.getInt("user_id", -1);
+
+        if (userId != -1 && ticket != null){
+            UserTicketQuery userTicketQuery = new UserTicketQuery(this);
+            userTicketQuery.saveTicket(userId, id, ticket);
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("img", clickedRes);
+            startActivity(intent);
+            Toast.makeText(this, "결제되었습니다.", Toast.LENGTH_SHORT).show();
+        } else{
+            Toast.makeText(this, "정보가 맞는지 확인해주세요.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
